37f04781c0cd5a92b3f43705ac11cc700561bb3e
shvveling <95494203+shvveling@users.noreply.github.com>
2025-05-31 23:03:33 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-
-# --- UmraJet Bot Configuration ---
+# ---- UmraJet Bot Configuration ----
 
 # Telegram bot token (replace with your actual bot token)
 BOT_TOKEN = "7667056220:AAEc8DwQ0WJrBfVk_yLN8wLWGpxUfRKT-5A"
 
-# Admin usernames (without '@')
-ADMINS = ["vip_arabiy", "V001VB"]
+# Admin usernames
+ADMIN_USERNAMES = ["vip_arabiy", "V001VB"]
 
 def is_admin(username):
-    return username in ADMINS
+    return username in ADMIN_USERNAMES
